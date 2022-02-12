d321ec44d882bcf39515d35bf9960d01571003b8
cesar becerra <sezaru@hotmail.es>
2022-02-12 15:42:44 -0500

    cambiado tipo de field del argumento numero_caja de la clase caja

--- backend/models.py
+++ backend/models.py
@@ -20,7 +20,7 @@ class sede (models.Model):
 
 class caja (models.Model):
 
-    numero_caja = models.IntegerField(validators=[MaxValueValidator(100)])
+    numero_caja = models.BigIntegerField(validators=[MaxValueValidator(100)])
     tipo = models.CharField(max_length=20)
 
 class sede_caja (models.Model):
