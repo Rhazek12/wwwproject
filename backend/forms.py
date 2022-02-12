4bfc7fd1439667d4e55e0a8efdc81ef819f93609
cesar becerra <sezaru@hotmail.es>
2022-02-12 15:23:53 -0500

    Terminado crud de cliente, caja, sede

--- backend/forms.py
+++ backend/forms.py
@@ -5,3 +5,14 @@ class usuario_form(forms.Form):
     rol = forms.CharField(max_length=20, required=True)
     nombre = forms.CharField(max_length=30, required=True)
     contrasena = forms.CharField(max_length=30, required=True)
+
+class cliente_form(forms.Form):
+    cedula = forms.IntegerField(max_value=10000000000000, required=True)
+
+class sede_form(forms.Form):
+    nombre =  forms.CharField(max_length=50, required=True)
+    direccion = forms.CharField(max_length=50, required=True)
+
+class caja_form(forms.Form):
+    numero_caja =  forms.IntegerField(max_value=100, required=True)
+    tipo = forms.CharField(max_length=20, required=True)
--- backend/views.py
+++ backend/views.py
@@ -6,8 +6,14 @@ from .forms import *
 
 # Create your views here.
 
+#SOLO PARA HACER PRUEBAS EN EL BACKEND:
+
 def busqueda_test(request):
     return render(request, "pruebas.html")
+
+#------------------------------------------------------------------------------------------
+
+#CRUD DE USUARIO:
 
 def busqueda_usuario (request):
 
@@ -86,6 +92,214 @@ def crear_usuario(request):
             return JsonResponse(True, safe=False)
         else:
             return JsonResponse(False, safe=False)
+    return JsonResponse(False, safe=False)
 
+#------------------------------------------------------------------------------------------
 
+#CRUD DE CLIENTE:
+
+def busqueda_cliente (request):
+
+    clientes =""
+
+    if(request.GET["cedula"]):
+        cedula_request = request.GET["cedula"]
+        clientes = list(cliente.objects.filter(cedula=cedula_request).values())
+    else:
+        clientes = list(cliente.objects.values())
+
+    return JsonResponse(clientes, safe=False)
+
+def editar_cliente(request):
+
+    if(request.GET["id"]):
+        id_cliente = request.GET["id"]
+        var_cliente =get_object_or_404(cliente, id = id_cliente)
+        datos = cliente_form(request.GET)
+
+        if datos.is_valid():
+            var_cliente.cedula = datos.cleaned_data['cedula']
+
+            var_cliente.save()
+
+            return JsonResponse(True, safe=False)
+        else:
+            return JsonResponse(False, safe=False)
+    else:
+        return JsonResponse(False, safe=False)
+        
+
+def eliminar_cliente(request):
+
+    if(request.GET["cedula"]):
+        cedula_cliente = request.GET["cedula"]
+        var_cliente =get_object_or_404(cliente, cedula = cedula_cliente)
+        var_cliente.delete()
+        return JsonResponse(True, safe=False)
+    else:
+        return JsonResponse(False, safe=False)
+        
+
+def crear_cliente(request):
+
+    datos = cliente_form()
+
+    if (request.method == "GET"):
+        datos = cliente_form(request.GET)
+
+        if datos.is_valid():
+            var_cliente = cliente()
+            var_cliente.cedula = datos.cleaned_data['cedula']
+
+            var_cliente.save()
+
+            return JsonResponse(True, safe=False)
+        else:
+            return JsonResponse(False, safe=False)
+
+    return JsonResponse(False, safe=False)
+
+#------------------------------------------------------------------------------------------
+
+#CRUD DE SEDE:
+
+def busqueda_sede (request):
+
+    sedes=""
+
+    if(request.GET["nombre"]):
+        nombre_request = request.GET["nombre"]
+        sedes = list(sede.objects.filter(nombre=nombre_request).values())
+    elif(request.GET["direccion"]):
+        direccion_request = request.GET["direccion"]
+        sedes = list(sede.objects.filter(direccion=direccion_request).values())
+    elif(request.GET["id"]):
+        id_request = request.GET["id"]
+        sedes = list(sede.objects.filter(id=id_request).values())
+    else:
+        sedes = list(sede.objects.values())
+
+    return JsonResponse(sedes, safe=False)
+
+def editar_sede(request):
+
+    if(request.GET["id"]):
+        id_sede = request.GET["id"]
+        var_sede =get_object_or_404(sede, id = id_sede)
+        datos = sede_form(request.GET)
+
+        if datos.is_valid():
+            var_sede.nombre = datos.cleaned_data['nombre']
+            var_sede.direccion = datos.cleaned_data['direccion']
+            var_sede.save()
+
+            return JsonResponse(True, safe=False)
+        else:
+            return JsonResponse(False, safe=False)
+    else:
+        return JsonResponse(False, safe=False)
+        
+
+def eliminar_sede(request):
+
+    if(request.GET["id"]):
+        id_sede = request.GET["id"]
+        var_sede =get_object_or_404(sede, id = id_sede)
+        var_sede.delete()
+        return JsonResponse(True, safe=False)
+    else:
+        return JsonResponse(False, safe=False)
+        
+
+def crear_sede(request):
+
+    datos = sede_form()
+
+    if (request.method == "GET"):
+        datos = sede_form(request.GET)
+
+        if datos.is_valid():
+            var_sede= sede()
+            var_sede.nombre = datos.cleaned_data['nombre']
+            var_sede.direccion = datos.cleaned_data['direccion']
+
+            var_sede.save()
+
+            return JsonResponse(True, safe=False)
+        else:
+            return JsonResponse(False, safe=False)
+
+    return JsonResponse(False, safe=False)
+#------------------------------------------------------------------------------------------
+
+#CRUD DE CAJA
+
+def busqueda_caja (request):
+
+    cajas=""
+
+    if(request.GET["numero_caja"]):
+        numero_caja_request = request.GET["numero_caja"]
+        cajas = list(caja.objects.filter(numero_caja=numero_caja_request).values())
+    elif(request.GET["tipo"]):
+        tipo_request = request.GET["tipo"]
+        cajas = list(caja.objects.filter(tipo=tipo_request).values())
+    elif(request.GET["id"]):
+        id_request = request.GET["id"]
+        cajas = list(caja.objects.filter(id=id_request).values())
+    else:
+        cajas = list(caja.objects.values())
+
+    return JsonResponse(cajas, safe=False)
+
+def editar_caja(request):
+
+    if(request.GET["id"]):
+        id_caja = request.GET["id"]
+        var_caja =get_object_or_404(caja, id = id_caja)
+        datos = caja_form(request.GET)
+
+        if datos.is_valid():
+            var_caja.numero_caja = datos.cleaned_data['numero_caja']
+            var_caja.tipo = datos.cleaned_data['tipo']
+            var_caja.save()
+
+            return JsonResponse(True, safe=False)
+        else:
+            return JsonResponse(False, safe=False)
+    else:
+        return JsonResponse(False, safe=False)
+        
+
+def eliminar_caja(request):
+
+    if(request.GET["id"]):
+        id_caja = request.GET["id"]
+        var_caja =get_object_or_404(caja, id = id_caja)
+        var_caja.delete()
+        return JsonResponse(True, safe=False)
+    else:
+        return JsonResponse(False, safe=False)
+        
+
+def crear_caja(request):
+
+    datos = caja_form()
+
+    if (request.method == "GET"):
+        datos = caja_form(request.GET)
+
+        if datos.is_valid():
+            var_caja= caja()
+            var_caja.numero_caja = datos.cleaned_data['numero_caja']
+            var_caja.tipo = datos.cleaned_data['tipo']
+
+            var_caja.save()
+
+            return JsonResponse(True, safe=False)
+        else:
+            return JsonResponse(False, safe=False)
+
+    return JsonResponse(False, safe=False)
+#------------------------------------------------------------------------------------------
 
