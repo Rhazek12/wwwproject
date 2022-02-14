efeddb3a65c633f4644719aedbd064e0eac47323
Santiago Cortés <david.cortes@correounivalle.edu.co>
2022-02-14 09:39:42 -0500

    Añadir front-end a la lista de sitios permitidos

--- www_project/settings.py
+++ www_project/settings.py
@@ -43,9 +43,9 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -57,10 +57,12 @@ ROOT_URLCONF = 'www_project.urls'
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
+    "https://vigorous-kirch-9a236d.netlify.app",
 ]
 
 CSRF_TRUSTED_ORIGINS = [
     "http://localhost:3000",
+    "https://vigorous-kirch-9a236d.netlify.app",
 ]
 
 TEMPLATES = [
