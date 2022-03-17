993b0ebd69f9d2848da11625dddeeac06a0d9b8f
cesar becerra <sezaru@hotmail.es>
2022-03-17 12:38:59 -0500

    cambiando links

--- www_project/settings.py
+++ www_project/settings.py
@@ -57,12 +57,12 @@ ROOT_URLCONF = 'www_project.urls'
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
-    "https://*vigorous-kirch-9a236d.netlify.app",
+    'https://*vigorous-kirch-9a236d.netlify.app',
 ]
 
 CSRF_TRUSTED_ORIGINS = [
     "http://localhost:3000",
-    "https://*vigorous-kirch-9a236d.netlify.app",
+    'https://*vigorous-kirch-9a236d.netlify.app',
 ]
 
 TEMPLATES = [
